4323d398190d40a049932be67774096007bbe0be
Salar Gholizadeh <salar.g@gmail.com>
2019-11-20 23:11:24 +0330

    first commit (contains header)

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="assets/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js"></script>
+    <title>Salar Gholizadeh</title>
+</head>
+<body>
+<div class="container container-page">
+    <header>
+        <div class="row">
+            <div class="col-12 col-md-auto order-md-1">
+                <div class="photo">
+                    <img src="content/avatar01.jpg" alt="photo">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="name h1">Salar Gholizadeh</div>
+                <div class="title mb-2">Software Developer</div>
+                <div class="description">I love crafting high quality apps on time.</div>
+            </div>
+        </div>
+    </header>
+    <div class="section-contact">
+
+    </div>
+</div>
+</body>
+</html>
